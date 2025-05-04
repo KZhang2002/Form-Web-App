36e41d765fae67d3312bfed202754c7f9c5bc2e8
EthanWZech <ethanwzech@gmail.com>
2025-05-04 07:16:50 -0500

    Added generated files from the backend.

---
+++ src/typing/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './FormControllerApi';
+export * from './FormTemplateControllerApi';
+export * from './UserControllerApi';
---
+++ src/typing/models/index.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ChangeUserPasswordRequest';
+export * from './CreateFormRequest';
+export * from './CreateFormTemplateContentRequest';
+export * from './CreateFormTemplateRequest';
+export * from './CreateFormTemplateSignatureRequest';
+export * from './CreateUserRequest';
+export * from './Form';
+export * from './FormContent';
+export * from './FormSignature';
+export * from './FormTemplate';
+export * from './FormTemplateContent';
+export * from './FormTemplateSignature';
+export * from './LoginCredential';
+export * from './SearchFormRequest';
+export * from './SignFormRequest';
+export * from './UpdateFormContentRequest';
+export * from './UserInfo';
